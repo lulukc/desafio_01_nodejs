058191a018b68e7111623cf3d60542f95795411f
Lucas Amancio <lhamancio@gmail.com>
2021-01-19 22:02:36 -0300

    arrumando teste

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { uuid, isUuid } = require('uuidv4');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
